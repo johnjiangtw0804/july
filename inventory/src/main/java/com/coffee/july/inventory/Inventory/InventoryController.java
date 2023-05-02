e7089d24ea798a6c7d9d26317849967ebf70da4b
Yyao5 <lisayyt@yahoo.com>
2023-05-01 17:17:46 -0700

    avoid cors

--- inventory/src/main/java/com/coffee/july/inventory/Inventory/InventoryController.java
+++ inventory/src/main/java/com/coffee/july/inventory/Inventory/InventoryController.java
@@ -8,10 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/inventory")
 public class InventoryController {
