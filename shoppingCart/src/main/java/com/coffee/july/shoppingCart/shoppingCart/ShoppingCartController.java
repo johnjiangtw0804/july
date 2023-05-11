745f2bc17eb4a896a3a942e267ba68aff2fda7cb
johnjiangtw0804 <johnjiangtw0804@gmail.com>
2023-05-10 18:03:07 -0700

    Update

--- shoppingCart/src/main/java/com/coffee/july/shoppingCart/shoppingCart/ShoppingCartController.java
+++ shoppingCart/src/main/java/com/coffee/july/shoppingCart/shoppingCart/ShoppingCartController.java
@@ -12,9 +12,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/shoppingCart")
 public class ShoppingCartController {
     Logger logger = LoggerFactory.getLogger(ShoppingCartController.class);
